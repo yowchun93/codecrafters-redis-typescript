9e2c3f57544386e98f8ac0ff09ed1398b8906f67
yowchun <yowchun93@gmail.com>
2024-11-24 13:20:12 +0800

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -5,9 +5,11 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
 const server: net.Server = net.createServer((connection: net.Socket) => {
-  connection.on("data", () => {
+  connection.on("data", (chunk: Buffer) => {
+    console.log(chunk.toString());
     connection.write("+PONG\r\n");
   });
+  // need to split the data
 });
 
 server.listen(6379, "127.0.0.1");
